f2b15f7e705470b07d274c787b5ce00121f0404d
Nonke <codernonke@163.com>
2021-08-16 18:21:56 +0800

    修改格式

--- src/error/error-types.js
+++ src/error/error-types.js
@@ -28,29 +28,6 @@ const errorTypes = {
     code: 401,
     message: '无操作权限'
   }
-
-  // // 动态内容为空
-  // DYNAMIC_CONTENT_IS_EMPTY: 'dynamic content is empty',
-
-
-
-
-
-  // // 用户不存在
-  // USER_DOES_NOT_EXIST: 'user does not exist',
-
-
-
-
-
-  // // 没有操作权限
-  // NO_OPERATION_PERMISSION: 'no operation permission',
-
-  // // 缺少参数
-  // MISSING_PARAMETER: 'missing parameter',
-
-  // // 不支持的参数类型
-  // UNSUPPORTED_PARAMETER_TYPE: 'unsupported parameter type'
 }
 
 module.exports = {
