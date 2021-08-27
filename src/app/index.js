88adb95af155b90aec21d720a9bab2def7a05c5d
Nonke <codernonke@163.com>
2021-08-27 10:41:49 +0800

    解决跨域

--- src/app/index.js
+++ src/app/index.js
@@ -2,9 +2,12 @@ const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
 const autoRegisterRouter = require('../router')
 const MhParameter = require('../common/MhParameter')
-
+const cors = require('koa2-cors')
 
 const app = new Koa()
+
+// 解决跨域
+app.use(cors())
 
 // 注册处理from data格式数据中间件
 app.use(bodyParser())
