f38121fb802f42f1fd58276963d1cdb70ed47f24
nonke <codernonke@163.com>
2021-09-05 20:26:51 +0800

    role开发中

--- src/router/role.js
+++ src/router/role.js
@@ -1,8 +1,9 @@
 const Router = require('koa-router')
 const { getRoleList } = require('../controller/role')
 const { handleRoleListParam } = require('../middleware/role')
+const { verifyAuth } = require('../middleware/auth')
 const RoleRouter = new Router({ prefix: '/role' })
 
-RoleRouter.get('/list', handleRoleListParam, getRoleList)
+RoleRouter.get('/list', verifyAuth, handleRoleListParam, getRoleList)
 
 module.exports = RoleRouter
