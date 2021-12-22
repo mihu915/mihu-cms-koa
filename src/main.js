423e69da0721305391f99bd5601028d9e6187ac9
misted <mihu0915@163.com>
2021-12-22 16:36:16 +0800

    test

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const config = require('./app/config')
 const { logger } = require('./app/logger')
 
 console.log(process.env.NODE_ENV)
-
+console.log('test')
 // 开启服务器
 app.listen(config.APP_PORT, config.APP_HOST, () => {
   logger.info(`服务器启动成功，端口号为：${config.APP_PORT}`)
